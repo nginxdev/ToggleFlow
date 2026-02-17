ad7caffe3e9d1e5fc439f4cd8c8c8f9f914d13b9
nginxdev <q4pradeep@gmail.com>
2026-02-17 15:48:19 +0100

    chore: Standardize code formatting and linting by configuring Prettier and Husky.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierPlugin from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -15,9 +17,17 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      ...eslintConfigPrettier.rules,
+      'react-refresh/only-export-components': 'off',
     },
   },
 ])
